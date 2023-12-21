073fc8349a6f0c5c75b182027a65eef4ab7d8ec9
Valentin Sigalov <vlgstudent036@gmail.com>
2023-12-21 00:39:09 -0500

    Added no notes found item

--- src/components/NotesList/components/NotesList.tsx
+++ src/components/NotesList/components/NotesList.tsx
@@ -1,27 +1,21 @@
 import NoteListItem from "@/components/NotesList/components/NoteListItem"
-import { useCategoriesStore } from "@/state/categoriesStore"
 import { useNotesStore } from "@/state/notesStore"
-import { activeScreenAtom } from "@/state/userAtoms"
 import { Note } from "@/types/noteTypes"
-import { useAtom } from "jotai"
 
 const NotesList = () => {
-  const { notes } =
-    useNotesStore()
+  const { notes } = useNotesStore()
 
-  return notes
-    .sort((a, b) => {
-      return (
-        (new Date((b).modified) as any) -
-        (new Date((a).modified) as any)
-      )
-    })
-    .map((note: Note) => (
-      <NoteListItem
-        key={note.id}
-        note={note}
-      />
-    ))
+  const sortedNotes = notes.sort((a, b) => {
+    return (new Date(b.modified) as any) - (new Date(a.modified) as any)
+  })
+
+  const numVisibleNotes = notes.filter((note) => note.visible).length
+
+  if (numVisibleNotes === 0) return <p className="no-notes-found">No notes found matching filter parameters.</p>
+
+  return sortedNotes.map((note: Note) => (
+    <NoteListItem key={note.id} note={note} />
+  ))
 }
 
 export default NotesList
